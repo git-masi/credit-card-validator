27dce60271d169274270d4bd0a89ff3b85d0b312
Eric Masi <emasi@vitusvet.com>
2023-07-18 19:26:28 -0400

    Add validation function

---
+++ packages/luhn-checksum/src/index.ts
@@ -0,0 +1,22 @@
+function luhnChecksum(code: string) {
+  const len = code.length;
+  const parity = len % 2;
+  let sum = 0;
+
+  for (let i = len - 1; i >= 0; i--) {
+    let d = parseInt(code.charAt(i));
+    if (i % 2 == parity) {
+      d *= 2;
+    }
+    if (d > 9) {
+      d -= 9;
+    }
+    sum += d;
+  }
+
+  return sum % 10;
+}
+
+export function validate(code: string) {
+  return luhnChecksum(code) === 0;
+}
