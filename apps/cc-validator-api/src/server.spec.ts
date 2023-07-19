050292f1ff6d3c273da2788cfc8fcfd5f18fda24
Eric Masi <emasi@vitusvet.com>
2023-07-18 20:34:04 -0400

    Pass the test

--- apps/cc-validator-api/src/server.spec.ts
+++ apps/cc-validator-api/src/server.spec.ts
@@ -5,7 +5,7 @@ import { describe, it } from "vitest";
 describe("server integration tests", () => {
   const app = createServer();
 
-  it("should return a 400 if there is no request body", async () => {
-    await supertest(app).post("/validate").expect(400);
+  it("should return status 400 if there is no request body", async () => {
+    await supertest(app).post("/credit-cards/validate").expect(400);
   });
 });
--- apps/cc-validator-api/src/server.ts
+++ apps/cc-validator-api/src/server.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from "express";
+import { z } from "zod";
+
+const reqBodySchema = z.object({
+  cardNumber: z.string(),
+});
 
 export function createServer() {
   const app: Express = express();
 
-  app.post("/validate", (req: Request, res: Response) => {
-    res.send("Hello world!");
+  app.post("/credit-cards/validate", (req: Request, res: Response) => {
+    const body = reqBodySchema.safeParse(req.body);
+
+    if (!body.success) {
+      res.status(400).send("Bad request");
+      return;
+    }
+
+    res.send("Ok");
   });
 
   return app;
